1d8ae36a3de4e94805c74272a22ac3d3f7692337
Esha rajpoot <esharajpoot156@gmail.com>
2026-06-22 03:57:55 +0500

    Attach Backend URL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "teyzix-market-production.up.railway.app",
   withCredentials: true, // cookies automatically jayengi
 });
 
