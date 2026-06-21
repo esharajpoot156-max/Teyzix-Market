0616f9789abd1b65504c1577386242a4b1d988e7
Esha rajpoot <esharajpoot156@gmail.com>
2026-06-22 04:25:00 +0500

    update URL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "teyzix-market-production.up.railway.app",
-  withCredentials: true, // cookies automatically jayengi
+  baseURL: "https://teyzix-market-production.up.railway.app/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
