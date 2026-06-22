3e847222840a60f58b4879ed9a804a08d4ca104b
Esha rajpoot <esharajpoot156@gmail.com>
2026-06-23 03:11:54 +0500

    change port

--- Backend/index.js
+++ Backend/index.js
@@ -38,7 +38,7 @@ app.use("/api/requests",requestRoute);
 app.use("/api/reviews",reviewRoute);
 app.use("/api/profiles",profileRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   await connectDB();
