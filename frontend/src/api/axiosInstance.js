0151fe98aa2811698f497c4ab9d2fd9050fd98be
Esha rajpoot <esharajpoot156@gmail.com>
2026-06-23 02:46:39 +0500

    update vercel in backend

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://teyzix-market-production.up.railway.app/api",
+  baseURL: "https://teyzix-market-backend.vercel.app/api",
   withCredentials: false,
 });
 
