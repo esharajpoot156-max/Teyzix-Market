828aee472e3e815b37c1c8822adeacf958fd5d5d
Esha rajpoot <esharajpoot156@gmail.com>
2026-09-11 01:57:27 +0500

    update

--- Backend/utilis/sendEmail.js
+++ Backend/utilis/sendEmail.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
+  family:4,
   auth: {
     user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_PASS,
