41d86f137d2b4b31b53747b72740c209785ea912
Esha rajpoot <esharajpoot156@gmail.com>
2026-09-10 06:01:51 +0500

    add render

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174"
+      "http://localhost:5174",
+      "https://teyzix-market.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
