9acc6d8c5a2fbd06351df778a6abec92841e271a
Esha rajpoot <esharajpoot156@gmail.com>
2026-09-11 01:30:18 +0500

    update

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://teyzix-market.onrender.com",
+  baseURL: "https://teyzix-market.onrender.com/api",
   withCredentials: false,
 });
 
