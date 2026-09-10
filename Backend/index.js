583684943da498459c95680136bcbcd90ceced04
Esha rajpoot <esharajpoot156@gmail.com>
2026-09-11 01:23:31 +0500

    update

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://teyzix-market.vercel.app/"
+      "https://teyzix-market.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
